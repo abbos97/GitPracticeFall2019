133a78f26c3a42aa5d14f5b1689221b3e984cbe7
abbos97 <abbosboev@gmail.com>
2020-05-17 11:31:50 -0400

    Added change

---
+++ IdeaProjects/Fall2019OnilneJDBC/src/test/java/com/jdbc/day1/BasicTest.java
@@ -0,0 +1,41 @@
+package com.jdbc.day1;
+
+import java.sql.*;
+
+public class BasicTest {
+    public static void main(String[] args) throws SQLException {
+        String URL = "jdbc:oracle:thin:@54.198.155.113:1521:xe";
+        String username = "hr";
+        String password = "hr";
+        //to establish connection with a database
+
+        Connection connection = DriverManager.getConnection(URL, username, password);
+
+        //ResultSet.TYPE_SCROLL_INSENSITIVE
+        //The constant indicating the type for a <code>ResultSet</code> object
+        //     * that is scrollable but generally not sensitive to changes to the data
+        //     * that underlies the <code>ResultSet</code>.
+        //ResultSet.CONCUR_READ_ONLY : The constant indicating the concurrency mode for a ResultSet</code> object that may NOT be updated.
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        //in executeQuery method we provide query as a parameter
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+        //resultSet.next() -- returns true until it reaches last row.
+        // and jumps to next row, if there is some row with data
+        while (resultSet.next()) {
+            //get data from 2nd column for every row
+            //2nd column is a first name info
+            System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) +
+                    " " + resultSet.getString(3));
+        }
+
+        resultSet.beforeFirst(); // to comeback to the beginning of result set
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("salary"));
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}

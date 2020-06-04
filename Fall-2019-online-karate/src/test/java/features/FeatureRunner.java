d89967c3282d5bed6fbb4e9ee8b1752f6e0ceb5e
abbos97 <abbosboev@gmail.com>
2020-06-03 20:27:53 -0400

    Added

---
+++ Fall-2019-online-karate/src/test/java/features/FeatureRunner.java
@@ -0,0 +1,14 @@
+package features;
+import com.intuit.karate.junit5.Karate;
+
+
+public class FeatureRunner {
+    @Karate.Test
+    Karate metaWeatherTest(){
+        return new Karate().tags("@meta_weather").relativeTo(getClass());
+    }
+    @Karate.Test
+    Karate bookIt(){
+        return new Karate().tags("@meta_weather").relativeTo(getClass());
+    }
+}
